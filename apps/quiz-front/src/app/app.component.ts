df8458441911e96ecfbbbae5556f98fe11fa6c44
BloodPixel <bloodpixellp@gmail.com>
2025-01-31 21:42:56 +0100

    access prototyp

--- apps/quiz-front/src/app/app.component.ts
+++ apps/quiz-front/src/app/app.component.ts
@@ -1,5 +1,7 @@
-import { Component } from '@angular/core';
+import { DOCUMENT } from '@angular/common';
+import { Component, Inject } from '@angular/core';
 import { AuthService } from '@auth0/auth0-angular';
+import { map, tap } from 'rxjs';
 
 @Component({
   selector: 'app-root',
@@ -7,5 +9,26 @@ import { AuthService } from '@auth0/auth0-angular';
   styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
-  constructor(private _auth: AuthService)
+  constructor(@Inject(DOCUMENT) private _document: Document, private _auth: AuthService) {
+    this.getToken();
+    this._auth.user$.pipe(tap((user)=> console.log(user))
+  ).subscribe();
+  }
+  
+  isAuthenticated$ = this._auth.isAuthenticated$;
+
+  login() {
+    this._auth.loginWithRedirect();
+  }
+
+  getToken() {
+    this._auth.getAccessTokenWithPopup({ authorizationParams: {
+      audience: "883DKSj9Avq2rfDGyoqcM0b1RZn6KAUF",
+    }}).subscribe((test)=> console.log(test)
+    )
+  }
+
+  logout() {
+    this._auth.logout({ logoutParams: { returnTo: this._document.location.origin }}).subscribe();
+  }
 }
